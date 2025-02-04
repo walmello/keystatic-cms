f4331d894b5cac97d7780a88b3e9282b4baa6155
Walter Melo <41174751+walmello@users.noreply.github.com>
2025-02-04 14:43:16 -0300

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: netlify(),
 });
